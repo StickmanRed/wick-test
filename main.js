eb325a83b918a8757135820665ba0053ea57b085
StickmanRed <100049599+StickmanRed@users.noreply.github.com>
2025-01-13 19:45:31 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 /* This code duplicates the normal Wick cursor. */
 const newCursor = new Wick.Tools.Cursor();
 newCursor.project = project;
-newCursor.paperTool = project._tools.cursor.paperTool;
 newCursor.name = 'newcursor'
 project._tools.newcursor = newCursor;
 

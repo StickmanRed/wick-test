340a1535d7ab3825918a8dc464554fa84dd21143
StickmanRed <100049599+StickmanRed@users.noreply.github.com>
2025-01-14 16:27:15 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,56 @@
+function start(item) {
+  this._ghost = this._buildGhost();
+  this._layer.addChild(this._ghost);
+
+  if(item.data.handleType === 'rotation') {
+    this.currentTransformation = 'rotate';
+  } else if (item.data.handleType === 'scale') {
+    this.currentTransformation = 'scale';
+  } else {
+    this.currentTransformation = 'translate';
+  }
+  
+  this._ghost.data.initialPosition = this._ghost.position;
+  this._ghost.data.scale = new paper.Point(1,1);
+
+  this.mod = {
+    needsInitiate: true
+  };
+}
+function update(item, e) {
+  if (this.currentTransformation === 'translate') {
+    this._ghost.position = this._ghost.position.add(e.delta);
+  }
+  else {
+    if (this.mod.needsInitiate) {
+      var lastPoint = e.point.subtract(e.delta);
+      this.mod.initialPoint = lastPoint;
+      if (this.currentTransformation === 'rotate') {
+        this.mod.action = 'rotate';
+      }
+      else if (item.data.handleEdge.includes('Center')) {
+        this.mod.action = 'resize-edge';
+      }
+      else {
+        this.mod.action = 'resize-corner';
+      }
+    }
+    console.log('hi'); // Placeholder
+  }
+}
+function finish(item) {
+  if(!this._currentTransformation) return;
+
+  this._ghost.remove();
+
+  if(this.currentTransformation === 'translate') {
+    var d = this._ghost.position.subtract(this._ghost.data.initialPosition);
+    this.translateSelection(d);
+  }
+
+  this._currentTransformation = null;
+}
+
 /* This code duplicates the normal Wick cursor. */
 const newCursor = new Wick.Tools.Cursor();
 newCursor.project = project;
@@ -13,7 +66,7 @@ newCursor.onMouseDrag = function (e) {
   if (this.hitResult.item && this.hitResult.item.data.isSelectionBoxGUI) {
     // Update selection drag
     if (!this._widget.currentTransformation) {
-      this._widget.startTransformation(this.hitResult.item);
+      this._widget.startTransformationMod(this.hitResult.item);
     }
     this._widget.updateTransformationMod(this.hitResult.item, e);
   } else if (this.selectionBox.active) {
@@ -22,7 +75,7 @@ newCursor.onMouseDrag = function (e) {
   } else if (this.hitResult.item && this.hitResult.type === 'fill') {
     // We're dragging the selection itself, so move the whole item.
     if (!this._widget.currentTransformation) {
-      this._widget.startTransformation(this.hitResult.item);
+      this._widget.startTransformationMod(this.hitResult.item);
     }
     this._widget.updateTransformationMod(this.hitResult.item, e);
   } else {
@@ -69,8 +122,9 @@ newCursor.onMouseUp = function (e) {
   }
 }
 
-paper.SelectionWidget.prototype.updateTransformationMod = paper.SelectionWidget.prototype.updateTransformation;
-paper.SelectionWidget.prototype.finishTransformationMod = paper.SelectionWidget.prototype.finishTransformation;
+paper.SelectionWidget.prototype.startTransformationMod = start;
+paper.SelectionWidget.prototype.updateTransformationMod = update;
+paper.SelectionWidget.prototype.finishTransformationMod = finish;
 
 /* Set up + activate newCursor */
 project._view._setupTools();
